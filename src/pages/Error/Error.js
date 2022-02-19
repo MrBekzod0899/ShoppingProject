834fd997b29bb84b8650e341207370135e334a3d
MrBekzod0899 <bekzodqurbonov1399@gmail.com>
2022-02-20 00:45:35 +0500

    changed

---
+++ src/pages/Error/Error.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import err1 from "../../asset/error2.gif"
+import './error.css'
+export default function Error() {
+  return (
+    <div className='errorPage'>   
+            <img src={err1} alt="no photos"/>       
+    </div>
+  )
+}
